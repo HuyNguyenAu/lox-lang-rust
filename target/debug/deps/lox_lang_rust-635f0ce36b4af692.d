4d83f3877b52cb1ba064dcc4d500a3897c72c767
Huy Nguyen <huynguyenau@gmail.com>
2021-05-15 14:07:04 +1000

    Delete target directory

--- target/debug/deps/lox_lang_rust-635f0ce36b4af692.d
+++
@@ -1,5 +0,0 @@
-/home/huy/Github/lox-lang-rust/target/debug/deps/lox_lang_rust-635f0ce36b4af692: src/main.rs
-
-/home/huy/Github/lox-lang-rust/target/debug/deps/lox_lang_rust-635f0ce36b4af692.d: src/main.rs
-
-src/main.rs:
--- target/debug/lox-lang-rust.d
+++
@@ -1,1 +0,0 @@
-/home/huy/Github/lox-lang-rust/target/debug/lox-lang-rust: /home/huy/Github/lox-lang-rust/src/main.rs
